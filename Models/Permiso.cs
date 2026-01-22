b0bea526b227bd25be140c28c1e9e7b6e7e6cd70
Carlos Aguirre <carlos.aguirre@it-seekers.com>
2026-01-22 17:33:40 -0600

    config initial

---
+++ Models/Permiso.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ComprasVentas.Models;
+
+public class Permiso
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; } = string.Empty;
+    public string? Recurso { get; set; }
+    public string Accion { get; set; }= string.Empty;
+}
