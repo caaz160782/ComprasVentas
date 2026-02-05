291f90134bdf24c5eeb6793586ed4fb50dbe5bab
Carlos Aguirre <carlos.aguirre@it-seekers.com>
2026-02-05 17:46:57 -0600

    end usuarios

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,54 @@
+using ComprasVentas.Dto;
+using ComprasVentas.Services.specification;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComprasVentas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioService _usuarioService;
+        public UsuarioController(IUsuarioService usuarioService)
+        {
+            _usuarioService = usuarioService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<UsuarioDto>>> GetAll()
+        {
+            return Ok(await _usuarioService.GetAllUserAsync());
+        } 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UsuarioDto>> GetById(int id)
+        {
+            var usuario = await _usuarioService.GetUsuarioByIdAsync(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+            return Ok(usuario);
+        }
+        [HttpPost]
+        public async Task<ActionResult<UsuarioDto>> Create(CreateUsuarioDto usuarioCreateDto)
+        {
+            var createdUsuario = await _usuarioService.CreateUsuarioAsync(usuarioCreateDto);
+            return StatusCode(StatusCodes.Status201Created);
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult<UsuarioDto>> Update(int id, CreateUsuarioDto usuarioUpdateDto)
+        {
+          await _usuarioService.UpdateUsuarioAsync(id, usuarioUpdateDto);
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)        {
+            await _usuarioService.DeleteUsuarioAsync(id);
+            return NoContent(); 
+        }
+
+
+
+    }
+}
