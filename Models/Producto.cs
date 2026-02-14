c570cbbeff62d14a4e0934e22f351580d947fbf8
Carlos Aguirre <caaz160782@gmail.com>
2026-02-13 23:59:43 -0600

    create new models

---
+++ Models/Producto.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace ComprasVentas.Models;
+
+public class Producto
+{
+    public int Id { get; set; }
+
+    public string Nombre { get; set; } = string.Empty;
+
+    public string? Descripcion { get; set; }
+
+    public string? UnidadMedida { get; set; }
+
+    public string? Marca { get; set; }
+
+    public decimal PrecioVentaActual { get; set; }
+
+    public string? Imagen { get; set; }
+
+    public bool Estado { get; set; }
+
+    public Categoria? Categoria { get; set; }
+
+    public ICollection<AlmacenProducto> AlmacenProductos { get; set; } = [];
+}
